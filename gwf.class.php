9b7dd5f688953007bef10dea8419d0aef2b50839
Dan Bennett <naebeth@googlemail.com>
2013-04-09 16:00:58 +0200

    started converting to extendable base class

---
+++ gwf.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class GoogleWebFonts extends LocalFonts_Base{
+	
+	// make and store the ZIP, return the URL
+	private function download_url(){
+		
+	}
+
+	// don't make a ZIP, move files straight to usable location
+	private function download_local(){
+		
+	}
+}
+
+?>
